cf02ec9c8d7aea272ca8428b1f785f187d414509
Tommogo-coder <nelsontommogo9@gmail.com>
2023-07-05 22:32:29 +0300

    HarvestHq

--- harvesthqapp/urls.py
+++ harvesthqapp/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path("", views.home, name="harvesthqapp-home"),
-        path("harvesthqapp/", views.about, name="harvesthqapp-about"),
+    path("", views.home, name="harvesthq-home"),
+        path("about/", views.about, name="harvesthq-about"),
 ]
